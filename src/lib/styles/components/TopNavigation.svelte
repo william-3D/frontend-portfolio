d503af35bc8a51fc0f6e2a18a1b2202f7d1a55d1
Sonia William <williamlaw2017@outlook.com>
2023-06-24 09:14:00 +0800

    set-up navbar

---
+++ src/lib/styles/components/TopNavigation.svelte
@@ -0,0 +1,60 @@
+<nav class="top-nav">
+	<a class="logo" href="">Sonia W</a>
+
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<a href="/projects">Projects</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+	</ul>
+</nav>
+
+<style lang="scss">
+	nav {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		line-height: 48px;
+		padding-left: 24px;
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		background-color: var(--paper);
+
+		.logo {
+			color: var(--primary);
+			font-weight: bold;
+			font-size: 16px;
+		}
+
+		a {
+			text-decoration: none;
+			color: var(--secondary);
+			&:hover {
+				color: var(--accent1);
+				text-decoration: underline;
+			}
+		}
+		ul {
+			list-style: none;
+			margin: 0;
+			padding: 0;
+			li {
+				display: inline-block;
+				a {
+					display: inline-block;
+					padding: 8px 16px;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
+	import TopNavigation from '$lib/styles/components/TopNavigation.svelte';
 	import '$lib/styles/main.scss';
 </script>
 
 <main>
+	<TopNavigation />
 	<slot />
 </main>
